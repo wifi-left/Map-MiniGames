8c555d236ad3374c7e18867b560fd8845eb7a0a1
Gamom <ysy1679682327@outlook.com>
2022-09-24 20:09:48 +0800

    Update

--- zip.ps1
+++ zip.ps1
@@ -1,7 +1,8 @@
 $version=Get-Date -Format 'yyyy.M.d (h:m:s)'
+$versionZ=Get-Date -Format 'yyyy.M.d'
 $versionD=Get-Date -Format 'yyyy.M.d.h.m.s'
 Add-Content -Path $env:GITHUB_ENV -Value "version=${version}"
 Add-Content -Path $env:GITHUB_ENV -Value "versionDetail=${versionD}"
 
 New-Item "../build" -ItemType Directory
-Compress-Archive -Path "." -DestinationPath "../build/MiniGames v$version.zip"
+Compress-Archive -Path "." -DestinationPath "../build/MiniGames v$versionZ.zip"
