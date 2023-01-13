208bb3f0be904256dae2d88e0a759436ca1807c3
wifi_left <ysy1679682327@outlook.com>
2023-01-13 21:36:31 +0800

    修改 GitHub Action

--- zip.ps1
+++ zip.ps1
@@ -51,14 +51,5 @@ function Out-FileUtf8NoBom {
 
 }
 
-echo "Writing the version number to the Datapack."
-"tellraw @s [`"\u00a7aMap Version: \u00a7b\u00a7l$version`"]" | Out-FileUtf8NoBom ".\datapacks\map_main\data\minecraft\functions\version\version.mcfunction"
-"say Map Version: $version" | Out-FileUtf8NoBom ".\datapacks\map_main\data\minecraft\functions\version\version1.mcfunction"
-
-echo "Adding the GitHub Environment."
 Add-Content -Path $env:GITHUB_ENV -Value "version=${version}"
 Add-Content -Path $env:GITHUB_ENV -Value "versionDetail=${versionD}"
-
-echo "Compressing the file."
-New-Item "../build" -ItemType Directory
-Compress-Archive -Path "." -DestinationPath "../build/MiniGames v$versionZ.zip"
